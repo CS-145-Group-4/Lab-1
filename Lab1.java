287c60d8c4d91f169eeb13779270d514618e833e
RDStudent <112741085+RDStudent@users.noreply.github.com>
2024-01-10 14:16:52 -0800

    Update Lab1.java
    Lot of changes here. Created intro to the game method based on lab requirements. Removed bestGuess method for single line of math.min command. Fixed higher/lower statements within game. added single guess game congratulations message. 
    
    After comparing output to Labs samples, changed the spacing for the results.

--- Lab1.java
+++ Lab1.java
@@ -5,33 +5,29 @@
 
 
 import java.util.*;
-//This constant dictates random number generation range for game
+
 //this program runs a command line based number guessing game
 public class Lab1 {
+
     //This constant dictates random number generation range for game
     public static final int RANGE_MAX = 100;
-    public static final int MAX_GUESSES = 9999; 
-    // bestGuess = MAX_GUESSES and then will call bestGame method after each game
 
-    //introduces the game. calls game method.
+    //Introduces the game. Initializes variables to be used for the results
+    //of gameplay, and sets variables based on gameplay analysis.
     public static void main(String[] args) {
+        intro();
+
         Scanner console = new Scanner(System.in);
-        
-        System.out.println("This program allows you to play a guessing game.");
-        System.out.println("I will think of a number between 1 and");
-        System.out.println("100 and will allow you to guess until");
-        System.out.println("you get it. For each guess, I will tell you");
-        System.out.println("whether the right answer is higher or lower");
-        System.out.println("than your guess.");
-        
+        int guessCount;
         int guessCountTotal = 0;
         int gameCount = 0;
-        int bestGuess = MAX_GUESSES;
+        int bestGuess = 0;
         String response = "y";
 
-        while (!response.startsWith("n")) {  //switched from .equals to .startsWith
-            int guessCount = game(console);
-            bestGuess = bestGame(guessCount, bestGuess);
+        while (!response.startsWith("n")) {
+            System.out.println();
+            guessCount = game(console);
+            bestGuess = Math.min(guessCount, bestGuess);
             guessCountTotal += guessCount;
             gameCount++;
 
@@ -41,64 +37,60 @@ public class Lab1 {
         results(bestGuess, guessCountTotal, gameCount);
     }
 
-    //while statements to starts a new game. generates a random
-    //number between 1 and 100. requests guess from player. calls inputChecker
-    //compares user guess to generated number. Counts guesses and games.
-    //congratulates player. When player chooses to end, displays results
-    public static int game(Scanner console) { // changing this to return an int
+    //Introduces the user to what the program does and how to play.
+    public static void intro() {
+        System.out.println("This program allows you to play a guessing game.");
+        System.out.println("I will think of a number between 1 and");
+        System.out.println("100 and will allow you to guess until");
+        System.out.println("you get it. For each guess, I will tell you");
+        System.out.println("whether the right answer is higher or lower");
+        System.out.println("than your guess.");
+    }
+
+    //runs the guessing game. takes in the console, returns number of guesses
+    //for each game.
+    public static int game(Scanner console) {
+        int userInput = 0;
         int guessCount = 0;
         Random rand = new Random();
-        int userInput;
-        int generatedNumber = 42;
-        System.out.println("\nI'm thinking of a number between " +
+        int generatedNumber = rand.nextInt(RANGE_MAX);
+        System.out.println("I'm thinking of a number between " +
                 "1 and " + RANGE_MAX + "...");
-
-        do {            // swapped to a do while for game      
-            System.out.print("Your guess? ");
+        while (userInput != generatedNumber) {
+            System.out.print("Your guess?");
             userInput = console.nextInt();
             guessCount++;
-
             if (userInput < generatedNumber) {
                 System.out.println("It's higher.");
             } else if (userInput > generatedNumber) {
                 System.out.println("It's lower.");
+            } else if (guessCount == 1) {
+                System.out.println("You got it right in " + guessCount
+                        + " guess");
             } else {
-                System.out.println("You got it right in " + guessCount + " guesses");
+                    System.out.println("You got it right in " + guessCount
+                            + " guesses");
             }
-        }  while (userInput != generatedNumber);
+        }
         return guessCount;
     }
 
-    // compares number of guesses from one game to the "best" guess
-    // reassigns value of bestGuess and returns
-    public static int bestGame(int guessCount, int bestGuess) {
-        if (guessCount <= bestGuess) {
-            return guessCount;
-        } else {
-            return bestGuess;
-        }
+    //This method analyzes game statistics and displays to user
+    public static void results(int bestGuess, int guessCountTotal, int gameCount) {
+        double average = (double) guessCountTotal / gameCount;
+        System.out.println("\nOverall results:");
+        System.out.printf("    %-13s = %d%n", "total games", gameCount);
+        System.out.printf("    %-13s = %d%n", "total guesses", guessCountTotal);
+        System.out.printf("    %-13s = %.1f%n", "guesses/game", average);
+        System.out.printf("    %-13s = %d%n", "best game", bestGuess);
     }
-
-    //This method analyzes game statistics, calculates average, and displays to user
-    public static void results(int bestGuess, int guessCountTotal, int gameCount){ // changed first param
-            double average = (double) guessCountTotal / gameCount;
-            System.out.println("\nOverall results:");
-            System.out.printf("\t%-13s = %d%n", "total games", gameCount);
-            System.out.printf("\t%-13s = %d%n", "total guesses", guessCountTotal);
-            System.out.printf("\t%-13s = %.1f%n", "guesses/game", average);
-            System.out.printf("\t%-13s = %d%n", "best game", bestGuess);
-    }
-
-    // assignment spec has indicated we can assume input will be an integer and in range
 
     //this method takes in the console, returns a string
     //determines if user wants to play another game by taking first letter of
-    //what the user types into console. if first letter isn't y/n, tells user
-    //to give a new command
+    //what the user types into console.
     public static String gameStatusCheck(Scanner console) {
         System.out.print("Do you want to play again? ");
         String userInput = console.next();
-        String response = userInput.toLowerCase();
-        return response;        
+        return userInput.toLowerCase();
     }
 }
